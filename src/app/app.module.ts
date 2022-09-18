c7e348d83076361a7059f4e0a4a68be1701ac719
Oliver <oliver721003@gmail.com>
2022-09-18 14:02:18 +0800

    feat(product): 產品資料建立至 json server 內 :triangular_flag_on_post:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -29,6 +30,7 @@ import { TaiwanDatePipe } from './taiwan-date/taiwan-date.pipe';
     MatIconModule,
     MatToolbarModule,
     MatSnackBarModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   declarations: [
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, mergeMap, Observable, toArray } from 'rxjs';
 
 import { Product } from '../model/product';
 
@@ -7,20 +8,21 @@ import { Product } from '../model/product';
   providedIn: 'root',
 })
 export class ProductService {
-  private readonly products = [
-    new Product({ id: 1, name: '產品 A', price: 999 }),
-    new Product({ id: 2, name: '產品 B', price: 200 }),
-    new Product({ id: 3, name: '產品 C', price: 10 }),
-    new Product({ id: 4, name: '產品 D', price: 150 }),
-  ];
+  private _url = 'http://localhost:3000/products';
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   getProduct(id: number): Observable<Product> {
-    return of(this.products.find((product) => product.id === id)!);
+    return this.httpClient
+      .get<Product>(`${this._url}/${id}`)
+      .pipe(map((product) => new Product(product)));
   }
 
   getProducts(): Observable<Product[]> {
-    return of(this.products);
+    return this.httpClient.get<Product[]>(this._url).pipe(
+      mergeMap((products) => products),
+      map((product) => new Product(product)),
+      toArray()
+    );
   }
 }
