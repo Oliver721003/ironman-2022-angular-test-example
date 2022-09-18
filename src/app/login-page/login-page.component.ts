12aca03ca9e0b74c4a44d691beabd3c148e15ecd
Oliver <oliver721003@gmail.com>
2022-09-18 14:04:57 +0800

    feat(login): 使用者登入頁面顯示實作 :triangular_flag_on_post:

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
-  constructor() {}
+  protected readonly form: FormGroup = this.fb.group({
+    id: this.fb.control<string | null>(null, {
+      validators: [Validators.required],
+    }),
+    password: this.fb.control<string | null>(null, {
+      validators: [Validators.required],
+    }),
+  });
+
+  get id(): FormControl<string | null> {
+    return this.form.get('id') as FormControl<string | null>;
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
 }
