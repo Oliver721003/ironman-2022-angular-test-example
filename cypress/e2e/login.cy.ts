c81f32bbcc4a0f9ebb9a1b576f35068f710ba084
Oliver <oliver721003@gmail.com>
2022-10-01 09:51:02 +0800

    refactor(login): 登入頁面 e2e 測試使用不同驗證方式 :recycle:

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -22,8 +22,18 @@ describe('使用者登入', () => {
   it('當輸入不存在帳號, 應顯示錯誤訊息, 且無法按下登入按鈕', () => {
     cy.get('button.mat-icon-button').contains('login').click();
     cy.get('input[type=text]').type('userB').blur();
-    cy.contains('此帳號不存在').should('exist');
+    // cy.contains('此帳號不存在').should('exist');
+    // cy.get('mat-error').should('have.text', '此帳號不存在');
+    // cy.get('mat-error').should('exist').and('have.text', '此帳號不存在');
+    cy.get('mat-error').should((target) => {
+      expect(target).to.exist;
+      expect(target).to.have.text('此帳號不存在');
+    });
     cy.get('button').contains('登入').parent().should('be.disabled');
+    // cy.get('button')
+    //   .contains('登入')
+    //   .parent()
+    //   .should('have.class', 'mat-button-disabled');
   });
 
   it('當輸入錯誤帳號密碼, 應登入失敗', () => {
