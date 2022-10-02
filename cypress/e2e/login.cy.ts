4a8011f727d458e849238e559d12a00ed53e5ae6
Oliver <oliver721003@gmail.com>
2022-10-02 15:49:15 +0800

    test(e2e): 使用 cypress fixture :recycle:

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -21,38 +21,44 @@ describe('使用者登入', () => {
     });
 
     it('當輸入不存在帳號, 應顯示錯誤訊息, 且無法按下登入按鈕', () => {
-      cy.get('button.mat-icon-button').contains('login').click();
-      cy.get('input[type=text]').type('userB').blur();
-      cy.get('mat-error').should('exist').and('have.text', '此帳號不存在');
-      cy.get('button').contains('登入').parent().should('be.disabled');
+      cy.fixture('users/id-not-exists').then(({ id, password }) => {
+        cy.get('button.mat-icon-button').contains('login').click();
+        cy.get('input[type=text]').type(id).blur();
+        cy.get('button').contains('登入').parent().should('be.disabled');
+        cy.get('mat-error').should('exist').and('have.text', '此帳號不存在');
+      });
     });
 
     it('當輸入錯誤帳號密碼, 應登入失敗', () => {
-      cy.get('button.mat-icon-button').contains('login').click();
-      cy.get('input[type=text]').type('userA');
-      cy.get('input[type=password]').type('00000');
-      cy.get('button')
-        .contains('登入')
-        .parent()
-        .click({ force: true })
-        .should('not.be.disabled');
-      cy.get('.mat-snack-bar-container')
-        .should('exist')
-        .and('have.text', '登入失敗');
+      cy.fixture('users/login-failed').then(({ id, password }) => {
+        cy.get('button.mat-icon-button').contains('login').click();
+        cy.get('input[type=text]').type(id);
+        cy.get('input[type=password]').type(password);
+        cy.get('button')
+          .contains('登入')
+          .parent()
+          .click({ force: true })
+          .should('not.be.disabled');
+        cy.get('.mat-snack-bar-container')
+          .should('exist')
+          .and('have.text', '登入失敗');
+      });
     });
 
     it('當輸入正確帳號密碼, 應登入成功', () => {
-      cy.get('button.mat-icon-button').contains('login').click();
-      cy.get('input[type=text]').type('userA');
-      cy.get('input[type=password]').type('1234567');
-      cy.get('button')
-        .contains('登入')
-        .parent()
-        .click({ force: true })
-        .should('not.be.disabled');
-      cy.get('.mat-snack-bar-container')
-        .should('exist')
-        .and('have.text', '登入成功');
+      cy.fixture('users/login-success').then(({ id, password }) => {
+        cy.get('button.mat-icon-button').contains('login').click();
+        cy.get('input[type=text]').type(id);
+        cy.get('input[type=password]').type(password);
+        cy.get('button')
+          .contains('登入')
+          .parent()
+          .click({ force: true })
+          .should('not.be.disabled');
+        cy.get('.mat-snack-bar-container')
+          .should('exist')
+          .and('have.text', '登入成功');
+      });
     });
   });
 });
