ec180de76e56a2227eb8787a1443ac5b1a8d5845
Oliver <oliver721003@gmail.com>
2022-10-03 15:47:16 +0800

    test(app): 新增標題列文字 e2e 測試程式 :white_check_mark:

--- cypress/e2e/application.cy.ts
+++ cypress/e2e/application.cy.ts
@@ -7,6 +7,13 @@ describe('2022 鐵人賽範例', () => {
     cy.title().should('eq', '2022 鐵人賽範例');
   });
 
+  it('顯示標題為 "2022 鐵人賽範例"', () => {
+    // cy.contains('2022 鐵人賽範例');
+    // cy.contains('.title', '2022 鐵人賽範例');
+    // cy.get('.title').contains('2022 鐵人賽範例');
+    cy.get('[color="primary"]').find('span').contains('2022 鐵人賽範例');
+  });
+
   specify('進入商品清單頁面', () => {
     cy.url().should('include', '/product/list');
   });
